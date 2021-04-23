aca076cb323dea38f887f32347f222c18a70cbfe
Borza Peter <peter.borza@anaplan.com>
2021-04-23 16:40:55 +0300

    app cleaned

--- src/App.js
+++ src/App.js
@@ -4,51 +4,6 @@ import { LinkContextProvider } from "./Context/link-provider";
 import Main from "./components/Main";
 import { BrowserRouter as Router } from "react-router-dom";
 
-// const links = [
-//     {
-//         path: "/",
-//         exact: true,
-//         component: HomePage,
-//         title: "Chuck Norris",
-//         id: 1,
-//     },
-//     {
-//         path: "/quiz",
-//         exact: false,
-//         component: Quiz,
-//         title: "Quiz",
-//         id: 2,
-//     },
-//     {
-//         path: "/building",
-//         exact: false,
-//         component: NewBuilding,
-//         title: "Building",
-//         id: 3,
-//     },
-//     {
-//         path: "/businesscard",
-//         exact: false,
-//         component: BusinessCard,
-//         title: "BusinessCard",
-//         id: 4,
-//     },
-//     {
-//         path: "/fruitcard",
-//         exact: true,
-//         component: FruitCard,
-//         title: "Fruits",
-//         id: 5,
-//     },
-//     {
-//         path: "/fruitcard/:id",
-//         component: Fruit,
-//         title: "",
-//         exact: false,
-//         id: 6,
-//     },
-// ];
-
 function App() {
     return (
         <div className="App">
